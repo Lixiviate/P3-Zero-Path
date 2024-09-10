5d9f182ae99cd52cbd41a1b30e5b356a26d46c20
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-09-10 06:23:01 -0600

    Add LandingPage logic

---
+++ tailwind.congif.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'subtle-teal': '#e6fafa',
+        },
+        fontFamily: {
+          serif: ['Cormorant Garamond', 'serif'],
+        },
+      },
+    },
+    plugins: [
+      require('@tailwindcss/forms'),
+    ],
+  }
