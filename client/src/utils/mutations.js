1bf487b8ebabb7ab12f57ca0784ae635fabe40c4
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-10-12 19:31:22 -0600

    Setup profile mutations and schemas

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,20 @@
 import { gql } from "@apollo/client";
 
+// Mutation to update a user's information
+export const UPDATE_USER = gql`
+  mutation updateUser($username: String, $email: String, $password: String) {
+    updateUser(username: $username, email: $email, password: $password) {
+      success
+      message
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
 // Login mutation accepts either a username or email via the `login` field
 export const LOGIN_USER = gql`
   mutation login($login: String!, $password: String!) {
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User } = require("../models");
 const { signToken, AuthenticationError } = require("../utils/auth");
+const bcrypt = require("bcrypt");
 
 const resolvers = {
   Query: {
@@ -35,9 +36,60 @@ const resolvers = {
     },
 
     addUser: async (parent, { username, email, password }) => {
+      // Check if username or email is already in use
+      const existingUser = await User.findOne({
+        $or: [{ username }, { email }],
+      });
+
+      if (existingUser) {
+        throw new AuthenticationError(
+          "Username or Email already exists. Please try again."
+        );
+      }
+
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
+    },
+
+    updateUser: async (parent, { username, email, password }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("You need to be logged in!");
+      }
+
+      // Check if the new username or email is already in use by another user
+      if (username || email) {
+        const existingUser = await User.findOne({
+          $or: [{ username }, { email }],
+          _id: { $ne: context.user._id },
+        });
+
+        if (existingUser) {
+          return {
+            success: false,
+            message: "Username or Email is already in use.",
+            user: null,
+          };
+        }
+      }
+
+      // Update user data
+      const updatedUser = await User.findById(context.user._id);
+
+      if (username) updatedUser.username = username;
+      if (email) updatedUser.email = email;
+      if (password) {
+        const saltRounds = 10;
+        updatedUser.password = await bcrypt.hash(password, saltRounds);
+      }
+
+      await updatedUser.save();
+
+      return {
+        success: true,
+        message: "User profile updated successfully",
+        user: updatedUser,
+      };
     },
   },
 };
