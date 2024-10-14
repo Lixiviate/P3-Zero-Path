93e3ccc9b33631a27d1643a072609b5a8eaac07c
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-10-13 19:00:22 -0600

    Removed old code, setup boilerplate for carbon interface api key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,11 @@
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
-export const searchGoogleBooks = (query) => {
-  return fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${
-      import.meta.env.VITE_GOOGLE_API_KEY
-    }`
-  );
+// Make a request to the Carbon Interface API
+export const getCarbonEmissions = (params) => {
+  return fetch("https://www.carboninterface.com/api/v1/estimates", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_CARBON_INTERFACE_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  });
 };
