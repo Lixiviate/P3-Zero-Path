07ae35268f3fc771b3f7ce77c3bb32fc0ab3dd29
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-10-21 18:53:15 -0600

    Updated CarbonCalculator

--- client/src/pages/Tracker.jsx
+++ client/src/pages/Tracker.jsx
@@ -6,7 +6,7 @@ const Tracker = () => {
       className="tracker-page min-h-screen"
       style={{ background: "linear-gradient(to bottom, #40E0D0, #4169E1)" }}
     >
-      <div className="container mx-auto px-4 py-8 flex justify-center items-center relative z-10 min-h-screen">
+      <div className="container mx-auto pt-20 px-4 py-8 flex justify-center items-center relative z-10">
         <CarbonCalculator />
       </div>
     </div>
